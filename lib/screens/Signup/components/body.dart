b9afa69a7c9e7ca6db3709a26ea5c95b7aa3f4ce
Mohnish nathani <nathanimohnish@gmail.com>
2020-12-02 22:04:36 +0530

    added loading for signin and signout

--- lib/screens/Signup/components/body.dart
+++ lib/screens/Signup/components/body.dart
@@ -10,87 +10,102 @@ import 'package:mydonationapp/components/rounded_password_field.dart';
 import 'package:mydonationapp/homePage.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:mydonationapp/services/auth.dart';
+import 'package:mydonationapp/shared/loading.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     final AuthService _auth = AuthService();
     String email, password;
-    return Background(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "SIGNUP",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: size.height * 0.03),
-            SvgPicture.asset(
-              "assets/icons/signup.svg",
-              height: size.height * 0.35,
-            ),
-            RoundedInputField(
-              hintText: "Your Email",
-              onChanged: (value) {
-                email = value;
-              },
-            ),
-            RoundedPasswordField(
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-            RoundedButton(
-              text: "SIGNUP",
-              press: () async {
-                dynamic result =
-                    await _auth.registerWithEmailAndPassword(email, password);
-                if (result != null) {
-                  print(result.uid);
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => HomePage()));
-                } else {
-                  print("Problem in signing in");
-                }
-              },
-            ),
-            SizedBox(height: size.height * 0.03),
-            AlreadyHaveAnAccountCheck(
-              login: false,
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginScreen();
+    bool loading = false;
+    return loading
+        ? Loading()
+        : Background(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "SIGNUP",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: size.height * 0.03),
+                  SvgPicture.asset(
+                    "assets/icons/signup.svg",
+                    height: size.height * 0.35,
+                  ),
+                  RoundedInputField(
+                    hintText: "Your Email",
+                    onChanged: (value) {
+                      email = value;
                     },
                   ),
-                );
-              },
+                  RoundedPasswordField(
+                    onChanged: (value) {
+                      password = value;
+                    },
+                  ),
+                  RoundedButton(
+                    text: "SIGNUP",
+                    press: () async {
+                      setState(() {
+                        loading = true;
+                      });
+                      dynamic result = await _auth.registerWithEmailAndPassword(
+                          email, password);
+                      if (result != null) {
+                        print(result.uid);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomePage()));
+                      } else {
+                        print("Problem in signing in");
+                        loading = false;
+                      }
+                    },
+                  ),
+                  SizedBox(height: size.height * 0.03),
+                  AlreadyHaveAnAccountCheck(
+                    login: false,
+                    press: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return LoginScreen();
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                  OrDivider(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SocalIcon(
+                        iconSrc: "assets/icons/facebook.svg",
+                        press: () {},
+                      ),
+                      SocalIcon(
+                        iconSrc: "assets/icons/twitter.svg",
+                        press: () {},
+                      ),
+                      SocalIcon(
+                        iconSrc: "assets/icons/google-plus.svg",
+                        press: () {},
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
-            OrDivider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SocalIcon(
-                  iconSrc: "assets/icons/facebook.svg",
-                  press: () {},
-                ),
-                SocalIcon(
-                  iconSrc: "assets/icons/twitter.svg",
-                  press: () {},
-                ),
-                SocalIcon(
-                  iconSrc: "assets/icons/google-plus.svg",
-                  press: () {},
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
+          );
   }
 }
