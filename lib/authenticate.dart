53db65825ea865629d1ef31589cb00dc2fa42831
Mohnish nathani <nathanimohnish@gmail.com>
2020-12-03 18:36:58 +0530

    completed firestore integration

--- lib/authenticate.dart
+++ lib/authenticate.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mydonationapp/homePage.dart';
 import 'package:flutter/material.dart';
+import 'package:mydonationapp/services/database.dart';
 import 'package:provider/provider.dart';
 import 'package:mydonationapp/models/user.dart';
 import 'package:mydonationapp/screens/Welcome/welcome_screen.dart';
@@ -13,12 +15,15 @@ class _LoginState extends State<Authenticate> {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
-
+    final dbusers = Provider.of<QuerySnapshot>(context);
     if (user == null) {
       print("user null");
       return WelcomeScreen();
     } else {
-      print(user);
+      // print(user);
+      for (var doc in dbusers.docs) {
+        print(doc.data());
+      }
       return HomePage();
     }
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mydonationapp/authenticate.dart';
 import 'package:mydonationapp/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:mydonationapp/services/database.dart';
 import 'package:provider/provider.dart';
 import 'package:mydonationapp/models/user.dart';
 
@@ -14,8 +16,11 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<User>.value(
-      value: AuthService().user,
+    return MultiProvider(
+      providers: [
+        StreamProvider<User>(create: (_) => AuthService().user),
+        StreamProvider<QuerySnapshot>(create: (_) => DatabaseService().users),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Authenticate(),
