83ea1ca5f62633011f1d0e01ca653292a0650b07
Girish Salunke <girishsalunke@Girishs-MacBook-Pro.local>
2020-12-03 19:27:02 +0530

    Added Toggle Switches

--- lib/authenticate.dart
+++ lib/authenticate.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mydonationapp/homePage.dart';
 import 'package:flutter/material.dart';
-import 'package:mydonationapp/services/database.dart';
+// import 'package:mydonationapp/services/database.dart';
 import 'package:provider/provider.dart';
 import 'package:mydonationapp/models/user.dart';
 import 'package:mydonationapp/screens/Welcome/welcome_screen.dart';
