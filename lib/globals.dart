78d0d182031fb45a87c36f6d8e4100113f32ce64
Mohnish nathani <nathanimohnish@gmail.com>
2020-12-05 19:31:26 +0530

    fixed all photo and type bugs

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,7 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-String imgurl = "nullurl";
-String uid = "nulluid";
+String imgurl;
+String uid;
+String type;
 
 DocumentReference userinst;
 DocumentSnapshot userdata;
+
+getdata() async {
+  userdata = await userinst.get();
+  imgurl = userdata.get('img');
+  type = userdata.get('type');
+}
