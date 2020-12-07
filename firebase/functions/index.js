6bde36ff486b82b3311871e92e85d57b30a6438f
Mohnish nathani <nathanimohnish@gmail.com>
2020-12-07 22:02:52 +0530

    added push notification

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -14,33 +14,40 @@ exports.offerTrigger = functions.firestore.document(
     "donors/{donorId}"
 ).onCreate((snapshot,context)=>{
     mssgData=snapshot.data();
+    admin.firestore().collection('users').doc(mssgData.ngouid).get()
+.then((snapshotreceiver)=>{
+    admin.firestore().collection('users').doc(mssgData.uid).get().then((snapshotsender)=>{
+        console.log("Receiver is "+snapshotreceiver.data().name);
+        console.log("Sender is "+snapshotsender.data().name);
+        var tokens=[];
+        tokens.push(snapshotreceiver.data().authid);
+        var payload = {
+            "notification":{
+                "title": "From " + snapshotsender.data().name,
+                "body": "Item: "+ mssgData.itemname,
+                "sound": "default",
+                // "image":"https://lh3.googleusercontent.com/a-/AOh14Gh2wZV5hAUJ4rJbc4Yb3_xb4oGiF3F8zjGOVbqQsPo=s96-c"
+            },
+            "data": {
+                "sendername" : snapshotsender.data().name,
+             "item": mssgData.itemname,
+            // },
+            // "fcm_options": {
+            //     "image":"https://firebasestorage.googleapis.com/v0/b/donationapp-89333.appspot.com/o/images%2F2020-12-06%2015%3A51%3A43.829036?alt=media&token=66112a12-5495-46cf-8065-a009ee98cb41"
+            }
+        }
+     return admin.messaging().sendToDevice(tokens,payload).then((response) => {
+         console.log("Notification Sent");
+        }).catch((err)=>{
+            console.log(err);
+        });
+    });
+});
     // console.log(mssgData.ngoname);
     // console.log(mssgData.itemname);
     // console.log(mssgData.message);
     // console.log(mssgData.quantity);
     // console.log(mssgData.ngoname);
-    var tokens=[];
-    tokens.push("eATUZ6fORHqjvyTUdvn0sF:APA91bEQV5rfh5oqsVNax2n9mI5LYITq-DtvqL9bJVGmSy6TdJwHYb1vN_POF7FWPWe0eK6th-VXWf5-BNQ6MqdfnPFK9P4NJ9eMNlQocEPxGiochkw3G-cGqAMi_Mxi_NI9g0Z8TNjy");
-    var payload = {
-        "notification":{
-            "title": "From" + mssgData.ngoname,
-            "body": "Item: "+ mssgData.itemname,
-            "sound": "default",
-            // "image":"https://lh3.googleusercontent.com/a-/AOh14Gh2wZV5hAUJ4rJbc4Yb3_xb4oGiF3F8zjGOVbqQsPo=s96-c"
-        },
-        "data": {
-            "sendername" : mssgData.ngoname,
-         "item": mssgData.itemname,
-        // },
-        // "fcm_options": {
-        //     "image":"https://firebasestorage.googleapis.com/v0/b/donationapp-89333.appspot.com/o/images%2F2020-12-06%2015%3A51%3A43.829036?alt=media&token=66112a12-5495-46cf-8065-a009ee98cb41"
-        }
-    }
- return admin.messaging().sendToDevice(tokens,payload).then((response) => {
-     console.log("Notification Sent");
-    }).catch((err)=>{
-        console.log(err);
-    });
 
 });
   
